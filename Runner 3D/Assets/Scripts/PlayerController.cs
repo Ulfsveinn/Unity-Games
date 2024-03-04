d4ec89f59b71a3ddbd996c66ebfa9a737f6d8b84
André Luis Mesquita Barbosa Júnior <128335061+Ulfsveinn@users.noreply.github.com>
2024-03-04 16:28:43 -0300

    Upd Comentários

--- Runner 3D/Assets/Scripts/PlayerController.cs
+++ Runner 3D/Assets/Scripts/PlayerController.cs
@@ -11,20 +11,22 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerRG = GetComponent<Rigidbody>();
+        playerRG = GetComponent<Rigidbody>();// Obtém o componente Rigidbody do jogador (serve para controlar o comportamento físico do jogador)
         Physics.gravity *= gravityModifier;//multiplica o valor da gravidade em cena
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)&&isOnGround){
-            playerRG.AddForce(Vector3.up*jumpForce,ForceMode.Impulse);
-            isOnGround = false;
+        if(Input.GetKeyDown(KeyCode.Space)&&isOnGround)// Se a tecla de espaço for pressionada e o jogador estiver no chão:
+        {
+            playerRG.AddForce(Vector3.up*jumpForce,ForceMode.Impulse);/*Aplica uma força que impulsiona o jogador para cima, com uma intensidade determinada pelo valor de jumpForce. 
+                                                                       * Esta força é aplicada instantaneamente, usando Impulse como método de aplicação da força.*/
+            isOnGround = false;//Define isOnGround como falso para indicar que o jogador não está mais no chão.
         }
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)// Quando ocorre uma colisão:
     {
-        isOnGround = true;
+        isOnGround = true;//Define isOnGround como verdadeiro para indicar que o jogador está no chão.
     }
 }
