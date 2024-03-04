126e8360a4b7068412612532df7f0e04e916c0ec
André Luis Mesquita Barbosa Júnior <128335061+Ulfsveinn@users.noreply.github.com>
2024-03-04 17:55:04 -0300

    Upd
    Criado o script spawnManager.

---
+++ Runner 3D/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPosition = new Vector3 (25f, 0.15f, 0f);
+    public float startDelay = 2f;
+    public float repeatRate = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab,spawnPosition,obstaclePrefab.transform.rotation);
+    }
+}
--- Runner 3D/Assets/Scripts/moveLeft.cs
+++ Runner 3D/Assets/Scripts/moveLeft.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class moveLeft : MonoBehaviour
 {
-    private float speed=30f;
+    private float speed=10f;
     // Start is called before the first frame update
     void Start()
     {
