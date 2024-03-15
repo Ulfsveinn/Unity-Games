e0b9706eb58fe50a1278e7c0cd66bcb333bdd1e3
André Luis Mesquita Barbosa Júnior <128335061+Ulfsveinn@users.noreply.github.com>
2024-03-15 18:08:37 -0300

    Upd

--- Runner 3D/Assets/Scripts/SpawnManager.cs
+++ Runner 3D/Assets/Scripts/SpawnManager.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclesPrefab;
     private Vector3 spawnPosition = new Vector3 (25f, 0.15f, 0f);
-    public float startDelay = 2f;
-    public float repeatRate = 2f;
+    private IEnumerator couroutine;
+    //public float startDelay = 2f;
+    //public float repeatRate = 2f;
     // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    void Start(){
+        //InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        couroutine = SpawnObstacles();
+        StartCoroutine(couroutine);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
     }
-    void SpawnObstacle()
-    {
-        Instantiate(obstaclePrefab,spawnPosition,obstaclePrefab.transform.rotation);
+    IEnumerator SpawnObstacles(){
+        while (true)
+        {
+            CreateObstacle();
+            yield return new WaitForSeconds(3f);//interrompa a execução da funçaõ dps de 3s ira voltar a executala.
+        }
+    }
+    void CreateObstacle(){
+        GameObject obstacle = obstaclesPrefab[Random.Range(0,obstaclesPrefab.Length-1)];
+        Instantiate(obstacle, spawnPosition, obstacle.transform.rotation);
     }
 }
